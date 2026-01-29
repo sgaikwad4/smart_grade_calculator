c80276bde365bea0cebcaaab2cca5a139d050570
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-01-29 11:36:23 -0800

    Created lists, and a function
    Created lists to store grades and amount of grades.

--- smart_grade_calculator.py
+++ smart_grade_calculator.py
@@ -1,7 +1,13 @@
 # grades lists
+assignments = []
+amount_of_assignments = []
 
 # caluclation function
-
+def calculation():
+    pass
 # Ask the amount of assignments
+amount_of_assignments.append(int(input("How many assignments do you have?")))
 
 # ask results in assignments
+for i in range(amount_of_assignments[0]):
+        assignments.append(int(input("Enter your grades:")))
