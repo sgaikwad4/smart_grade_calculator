067e40659697f2818db1a1aabbcd69e7402ef040
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-01-30 08:47:06 -0800

    Called function and made function do math
    Made a variable named "average" which equals the sum of assignments divided by the amount of assignments

--- smart_grade_calculator.py
+++ smart_grade_calculator.py
@@ -4,10 +4,15 @@ amount_of_assignments = []
 
 # caluclation function
 def calculation():
-    pass
+    average = sum(assignments)/amount_of_assignments[0]
+    if average<49:
+        print(f"Your grade is an F at {average}%")
 # Ask the amount of assignments
 amount_of_assignments.append(int(input("How many assignments do you have?")))
 
 # ask results in assignments
 for i in range(amount_of_assignments[0]):
         assignments.append(int(input("Enter your grades:")))
+        
+# call caluclation function
+calculation()
