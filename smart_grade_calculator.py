d0d6ef0f5cf766bedc3a5ca13e54627946b3dded
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-01-30 09:46:18 -0800

    Added the rest of the letter grades
    Added the rest of the letter grades.

--- smart_grade_calculator.py
+++ smart_grade_calculator.py
@@ -10,13 +10,27 @@ def calculation():
         print(f"Your grade is an F at {average}%")
         
     elif 50<=average<=59:
-        print(f"You're not failing {average}")
+        print(f"Your grade is a C- at {average}%")
+        
+    elif 60<=average<=66:
+        print(f"Your grade is a C at {average}%")
+        
+    elif 67<=average<=72:
+        print(f"Your grade is a C+ at {average}%")
+        
+    elif 73<=average<=85:
+        print(f"Your grade is a B at {average}%")
+    
+    else:
+        print(f"Your grade is an A at {average}%")
+    
 # Ask the amount of assignments
-amount_of_assignments.append(int(input("How many assignments do you have?")))
+while True:
+    amount_of_assignments.append(int(input("How many assignments do you have?")))
 
 # ask results in assignments
 for i in range(amount_of_assignments[0]):
-        assignments.append(int(input("Enter your grades:")))
+    assignments.append(int(input("Enter your grades:")))
         
 # call caluclation function
 calculation()
