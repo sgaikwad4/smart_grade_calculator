5cc77730c3c64eccbd3d97c776f35ea0efe4bcde
sgaikwad4 <siddhantg612@deltalearns.ca>
2026-01-30 09:05:29 -0800

    Rouned the average
    When entering grades that average out between two letter grades the average wouldn't choose a grade. For example: if your average was a 49.5% the average is neither smaller than 49 or larger then 50. So I rounded the average.

--- smart_grade_calculator.py
+++ smart_grade_calculator.py
@@ -5,8 +5,12 @@ amount_of_assignments = []
 # caluclation function
 def calculation():
     average = sum(assignments)/amount_of_assignments[0]
-    if average<49:
+    average = round(average)
+    if average<=49:
         print(f"Your grade is an F at {average}%")
+        
+    elif 50<=average<=59:
+        print(f"You're not failing {average}")
 # Ask the amount of assignments
 amount_of_assignments.append(int(input("How many assignments do you have?")))
 
